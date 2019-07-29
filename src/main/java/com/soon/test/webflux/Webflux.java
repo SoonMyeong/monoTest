28cc56f52310926ee344cd56570bb85a31d94a78
soon <smkwon@neonexsoft.com>
2019-07-29 15:49:03 +0900

    test

--- src/main/java/com/soon/test/webflux/Webflux.java
+++ src/main/java/com/soon/test/webflux/Webflux.java
@@ -1,5 +1,7 @@
 package com.soon.test.webflux;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,20 +24,31 @@ public class Webflux {
 
     @PostMapping(value = "/send")
     public void sending(){
-        sendMessage("Test");
+        String test = "{Mono Test}";
+        sendMessage(test);
     }
 
 
     public void sendMessage(String message){
         WebClient webClient = WebClient.builder().build();
 
-        Mono<String> sendMesssage = webClient.post().uri("http://localhost:9088")
+        Mono<String> sendMesssage = webClient.post().uri("http://192.168.1.198:8088")
+                                    .header("dstMRN","urn:mrn:mcl:vessel:dma:poul-lowenorn")
+                                    .header("srcMRN","a")
                                     .syncBody(message).retrieve().bodyToMono(String.class);
+        //push
 
-        sendMesssage.subscribe(s->{
-            System.out.println("sending message..");
-        });
-
+        sendMesssage.subscribe(
+                s->{
+                        System.out.println("sending message..");
+                    }, //onNext(T) 값이 넘어 올 때 출력
+                error->{
+                    System.err.println("sending error");
+                }, // 에러 발생시 출력
+                ()->{
+                    System.out.println("send complete");
+                }// 정상 종료시 출력
+        );
 
     }
 
